e8fddebe581f51a20b9178904e9ce87fdeccd258
Pratham Agrawal <pratham.agrawal2020@vitstudent.ac.in>
2023-02-12 02:01:28 +0530

    Fix edge cases

--- server/app.py
+++ server/app.py
@@ -34,113 +34,120 @@ def index():
     f = open('./notebooks/package.json')
     loader = json.load(f)
     # selection = loader['selection']
-    selection = request.json['selection']
-    data = loader['data']
-    data = pd.DataFrame.from_dict(data, orient='columns')
-    subset = list(data[selection])
-
+    
     # schema = loader['schema']
-    schema = request.json['schema']
-    schema = pd.DataFrame.from_dict(schema, orient='columns')
+    if(request.json['rules']==[]):
+        return {}
+    else:
+        schema = request.json['rules']
+        schema = pd.DataFrame.from_dict(schema, orient='columns')
 
-    full = schema[schema['match'] == 'full']
-    full = list(full['id'])
-    partial = schema[schema['match'] == 'partial']
-    partial = list(partial['id'])
+        selection = schema['id']
+        data = loader['data']
+        data = pd.DataFrame.from_dict(data, orient='columns')
+        subset = list(data[selection])
 
-    # full = ['STATUS', 'PRODUCTCODE']
-    # partial = ['QUANTITYORDERED', 'PRICEEACH', 'SALES', 'PRODUCTLINE',
-    #            'MSRP', 'CITY', 'STATE', 'CONTACTLASTNAME', 'CONTACTFIRSTNAME']
+        full = schema[schema['match'] == 'full']
+        full = list(full['id'])
+        partial = schema[schema['match'] == 'partial']
+        partial = list(partial['id'])
+        
 
-    subset = data[selection]
-    data = subset.copy()
+        # full = ['STATUS', 'PRODUCTCODE']
+        # partial = ['QUANTITYORDERED', 'PRICEEACH', 'SALES', 'PRODUCTLINE',
+        #            'MSRP', 'CITY', 'STATE', 'CONTACTLASTNAME', 'CONTACTFIRSTNAME']
 
-    """Processing the FULL Dependencies variables"""
+        subset = data[selection]
+        data = subset.copy()
+        std = MinMaxScaler()
 
-    Label_encoder = LabelEncoder()
-    for item in full:
-        subset[item] = Label_encoder.fit_transform(subset[item])
+        """Processing the FULL Dependencies variables"""
+        Label_encoder = LabelEncoder()
+        for item in full:
+            if(subset.dtypes[item]=='object'):
+                subset[item] = Label_encoder.fit_transform(subset[item])
+            else:
+                subset[[item]] = std.fit_transform(subset[[item]])
 
-    """Processing the Partial Dependencies Variables"""
-    categorial = []
-    std = MinMaxScaler()
-    numerical = []
-    for item in partial:
-        if (subset.dtypes[item] == 'object'):
-            categorial.append(item)
-        else:
-            subset[[item]] = std.fit_transform(subset[[item]])
 
-    subset = pd.get_dummies(subset, columns=categorial)
+        """Processing the Partial Dependencies Variables"""
+        categorial = []
+        for item in partial:
+            if (subset.dtypes[item] == 'object'):
+                categorial.append(item)
+            else:
+                subset[[item]] = std.fit_transform(subset[[item]])
 
-    pca = PCA(n_components=2, random_state=42)
-    subset_pd = pca.fit_transform(subset)
+        subset = pd.get_dummies(subset, columns=categorial)
 
-    wcss = []
-    for i in range(2, 10):
-        kmeans = KMeans(n_clusters=i, init="k-means++",
+        pca = PCA(n_components=2, random_state=42)
+        subset_pd = pca.fit_transform(subset)
+
+        wcss = []
+        for i in range(2, 10):
+            kmeans = KMeans(n_clusters=i, init="k-means++",
+                            max_iter=500, n_init=10, random_state=123)
+            kmeans.fit(subset_pd)
+            labels = kmeans.labels_
+            wcss.append(silhouette_score(subset_pd, labels, metric='euclidean'))
+
+        elbowval = wcss.index(max(wcss))+2
+
+        kmeans = KMeans(n_clusters=2, init="k-means++",
                         max_iter=500, n_init=10, random_state=123)
-        kmeans.fit(subset_pd)
-        labels = kmeans.labels_
-        wcss.append(silhouette_score(subset_pd, labels, metric='euclidean'))
+        identified_clusters = kmeans.fit_predict(subset_pd)
 
-    elbowval = wcss.index(max(wcss))+2
+        data_with_clusters = pd.DataFrame(data).copy()
+        data_with_clusters['Cluster'] = identified_clusters
 
-    kmeans = KMeans(n_clusters=2, init="k-means++",
-                    max_iter=500, n_init=10, random_state=123)
-    identified_clusters = kmeans.fit_predict(subset_pd)
+        # fig = px.scatter(data_with_clusters, x = "SALES", y="PRODUCTCODE",
+        #           color='Cluster', opacity = 0.8,size_max=30,template="plotly_dark",hover_name="STATUS")
+        # fig.show()
 
-    data_with_clusters = pd.DataFrame(data).copy()
-    data_with_clusters['Cluster'] = identified_clusters
+        # categorical = []
 
-    # fig = px.scatter(data_with_clusters, x = "SALES", y="PRODUCTCODE",
-    #           color='Cluster', opacity = 0.8,size_max=30,template="plotly_dark",hover_name="STATUS")
-    # fig.show()
+        # std = MinMaxScaler()
 
-    # categorical = []
+        # for item in selection:
+        #     if(subset.dtypes[item] == "object"):
+        #         categorical.append(item)
+        #         subset[item] = subset[item].apply(str.lower)
+        #     else:
+        #         subset[[item]] = std.fit_transform(subset[[item]])
 
-    # std = MinMaxScaler()
+        # for item in full:
+        #     if(item in categorical):
+        #         categorical.remove(item)
 
-    # for item in selection:
-    #     if(subset.dtypes[item] == "object"):
-    #         categorical.append(item)
-    #         subset[item] = subset[item].apply(str.lower)
-    #     else:
-    #         subset[[item]] = std.fit_transform(subset[[item]])
+        # """Processing the FULL Dependencies variables"""
+        # Label_encoder = LabelEncoder()
+        # for item in full:
+        #     subset[item] = Label_encoder.fit_transform(subset[item])
 
-    # for item in full:
-    #     if(item in categorical):
-    #         categorical.remove(item)
+        # """Processing the Partial Dependencies Variables"""
+        # subset=pd.get_dummies(subset,columns=categorical)
 
-    # """Processing the FULL Dependencies variables"""
-    # Label_encoder = LabelEncoder()
-    # for item in full:
-    #     subset[item] = Label_encoder.fit_transform(subset[item])
+        # wcss = []
+        # for i in range(2, 10):
+        #     kmeans = KMeans(n_clusters=i, init="k-means++",
+        #                     max_iter=500, n_init=10, random_state=123)
+        #     kmeans.fit(subset)
+        #     labels = kmeans.labels_
+        #     wcss.append(silhouette_score(subset, labels, metric='euclidean'))
 
-    # """Processing the Partial Dependencies Variables"""
-    # subset=pd.get_dummies(subset,columns=categorical)
+        # elbowval = wcss.index(max(wcss))+1
+        # print(elbowval)
 
-    # wcss = []
-    # for i in range(2, 10):
-    #     kmeans = KMeans(n_clusters=i, init="k-means++",
-    #                     max_iter=500, n_init=10, random_state=123)
-    #     kmeans.fit(subset)
-    #     labels = kmeans.labels_
-    #     wcss.append(silhouette_score(subset, labels, metric='euclidean'))
+        # kmeans = KMeans(n_clusters=elbowval, init="k-means++",
+        #                 max_iter=500, n_init=10, random_state=123)
+        # identified_clusters = kmeans.fit_predict(subset)
 
-    # elbowval = wcss.index(max(wcss))+1
-    # print(elbowval)
+        # data_with_clusters = pd.DataFrame(subset).copy()
+        # data_with_clusters['Cluster'] = identified_clusters
 
-    # kmeans = KMeans(n_clusters=elbowval, init="k-means++",
-    #                 max_iter=500, n_init=10, random_state=123)
-    # identified_clusters = kmeans.fit_predict(subset)
+        # print(data_with_clusters)
 
-    # data_with_clusters = pd.DataFrame(subset).copy()
-    # data_with_clusters['Cluster'] = identified_clusters
-
-    # print(data_with_clusters)
-
-    return data_with_clusters.to_dict()
+        return data_with_clusters.to_dict()
 
 
 if __name__ == '__main__':
