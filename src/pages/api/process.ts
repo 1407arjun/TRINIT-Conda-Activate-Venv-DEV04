35c43c0ea63b9535c53c2b85394fe5beb0f81005
Arjun Sivaraman <arjun140702@gmail.com>
2023-02-16 00:47:36 +0530

    Update API endpoint for flask server

--- src/pages/api/process.ts
+++ src/pages/api/process.ts
@@ -5,11 +5,14 @@ const process = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === "POST") {
         const { rules, count, endpoint } = req.body
         try {
-            const { data } = await axios.post("http://127.0.0.1:5000", {
-                rules,
-                count,
-                endpoint
-            })
+            const { data } = await axios.post(
+                "https://clusterbase-flask.onrender.com",
+                {
+                    rules,
+                    count,
+                    endpoint
+                }
+            )
             res.status(200).send(data)
         } catch (e) {
             console.log(e)
